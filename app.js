d9fefe08e52e93a32a9e9645cdc17d114e9e8123
Andres Soria <andres.zov@gmail.com>
2022-09-03 17:10:32 -0500

    Add movie API and test it with an async function

---
+++ app.js
@@ -0,0 +1,16 @@
+const API_URL =
+  "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bb89e2f4e932b43199f99e55b0ee957e&page=1";
+
+const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
+const SEARCH_URL =
+  'https://api.themoviedb.org/3/search/movie?api_key=bb89e2f4e932b43199f99e55b0ee957e&query="';
+
+//Get initial movies
+getMovies(API_URL);
+
+async function getMovies(url) {
+  const res = await fetch(url);
+  const data = await res.json();
+
+  console.log(data.results);
+}

68e76be304e7e018359c9d01b00cfac346723223
Andres Soria <andres.zov@gmail.com>
2022-09-03 17:15:20 -0500

    Implemet function to search movies

--- app.js
+++ app.js
@@ -2,8 +2,11 @@ const API_URL =
   "https://api.themoviedb.org/3/discover/movie?sort_by=popularity.desc&api_key=bb89e2f4e932b43199f99e55b0ee957e&page=1";
 
 const IMG_PATH = "https://image.tmdb.org/t/p/w1280";
-const SEARCH_URL =
+const SEARCH_API =
   'https://api.themoviedb.org/3/search/movie?api_key=bb89e2f4e932b43199f99e55b0ee957e&query="';
+
+const form = document.getElementById("form");
+const search = document.getElementById("search");
 
 //Get initial movies
 getMovies(API_URL);
@@ -14,3 +17,14 @@ async function getMovies(url) {
 
   console.log(data.results);
 }
+
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+  const searchTerm = search.value;
+  if (searchTerm && search !== "") {
+    getMovies(SEARCH_API + searchTerm);
+    search.value = "";
+  } else {
+    window.location.reload();
+  }
+});
